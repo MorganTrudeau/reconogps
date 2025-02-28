41174920d3899b0f4aa8f20babcc5042af9868ae
Morgan Trudeau <morgantrudeau@hotmail.com>
2025-02-28 14:26:59 -0800

    Updated splash

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import "react-native-gesture-handler";
 
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  DefaultTheme,
+  NavigationContainer,
+  Theme,
+} from "@react-navigation/native";
 import ThemeProvider from "./context/ThemeContext";
 import NavigationStack from "./navigation";
 import { StatusBar } from "expo-status-bar";
@@ -20,15 +24,28 @@ import { MapLayerProvider } from "./context/MapLayerContext";
 import MessagingManager from "./firebase/MessagingManager";
 import { DynamicAssetDataLoader } from "./services/DynamicAssetDataLoader";
 import { hide } from "react-native-bootsplash";
+import { colors } from "./styles";
 
 const hideSplash = () => hide({ fade: true });
 
+const navigationTheme: Theme = {
+  ...DefaultTheme,
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: colors.primary,
+    background: colors.background,
+  },
+};
+
 export default function App() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <NavigationContainer onReady={hideSplash}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
+    <GestureHandlerRootView
+      style={{ flex: 1, backgroundColor: colors.background }}
+    >
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer onReady={hideSplash} theme={navigationTheme}>
             <SafeAreaProvider>
               <ThemeProvider>
                 <ToastProvider>
@@ -40,7 +57,7 @@ export default function App() {
                     >
                       <PopoverProvider>
                         <MapLayerProvider>
-                          <StatusBar style="light" />
+                          {/* <StatusBar style="light" /> */}
                           <NavigationStack />
                         </MapLayerProvider>
                       </PopoverProvider>
@@ -54,9 +71,9 @@ export default function App() {
                 <DynamicAssetDataLoader />
               </ThemeProvider>
             </SafeAreaProvider>
-          </PersistGate>
-        </Provider>
-      </NavigationContainer>
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
     </GestureHandlerRootView>
   );
 }
